e87642fd8ca6bf2e46f6eb594a51eb251f974179
zachvlat <zachariasvlatakis@gmail.com>
2023-11-17 16:49:42 +0200

    safearea

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Provider as PaperProvider, Appbar } from 'react-native-paper';
 import Fab from './components/Fab';
 import MainTabs from './components/MainTabs';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Appbar.Header>
-        <Appbar.Content title="ScoopNite" />
-      </Appbar.Header>
-      <MainTabs></MainTabs>
-      <Fab></Fab>
-    </PaperProvider>
+    <SafeAreaView>
+      <PaperProvider>
+        <Appbar.Header>
+          <Appbar.Content title="ScoopNite" />
+        </Appbar.Header>
+        <MainTabs></MainTabs>
+        <Fab></Fab>
+      </PaperProvider>
+    </SafeAreaView>
   );
 }
