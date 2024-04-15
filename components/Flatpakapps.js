5770a4d9fd8607a86faf6f658ae3facf123e3390
zachvlat <zachariasvlatakis@gmail.com>
2024-04-15 14:37:36 +0300

    axios + some padding bottom

--- components/Flatpakapps.js
+++ components/Flatpakapps.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
 import Appcard from './Appcard';
-import appData from '../appData.json';
+import axios from 'axios';
 
 export default function Flatpakapps() {
+  const [appData, setAppData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://raw.githubusercontent.com/zachvlat/wingetNite/master/appData.json');
+      setAppData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <ScrollView>
+    <ScrollView contentContainerStyle={styles.scrollView}>
       {appData.map((app, index) => (
         <Appcard
           key={index}
@@ -16,3 +31,9 @@ export default function Flatpakapps() {
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    paddingBottom: 80,
+  },
+});
